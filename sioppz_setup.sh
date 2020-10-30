afce6e8c470a2996d01a50f238a631d848cf5cd9
sioppz <alfred.tsaizer@gmail.com>
2020-10-30 14:08:36 +0400

    Add files via upload

---
+++ sioppz_setup.sh
@@ -0,0 +1,28 @@
+# switch touchpad to natural scrolling
+sudo apt update
+
+#undervolt
+sudo apt install python3-pip
+sudo pip install undervolt
+sudo undervolt --core -130 --cache -130
+#persistence details here https://github.com/georgewhewell/undervolt
+
+#system name
+sudo nano /etc/hostname
+
+#tools foss
+sudo apt install blender gimp inkscape zim system76-cuda-latest natron
+
+#insync
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
+echo 'deb http://apt.insync.io/ubuntu groovy non-free contrib' | sudo tee /etc/apt/sources.list.d/insync.list
+sudo apt update
+sudo apt install insync
+
+
+#houdini
+#https://www.sidefx.com/download/houdini-for-linux/ - latest
+#https://www.sidefx.com/download/download-houdini/63948/ - RS compatible
+
+#redshift3d --last licensed version
+#https://www.redshift3d.com/forums/viewthread/29535/
